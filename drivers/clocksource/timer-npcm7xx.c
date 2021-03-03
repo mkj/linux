b82d0da116f865d8f1576c11142d6326b8a68ec1
Tomer Maimon <tmaimon77@gmail.com>
2021-03-03 17:43:01 +0200

    clocksource: npcm: Add NPCM8xx timer support
    Add Nuvoton BMC NPCM8xx timer support.
    
    Signed-off-by: Tomer Maimon <tmaimon77@gmail.com>

--- drivers/clocksource/timer-npcm7xx.c
+++ drivers/clocksource/timer-npcm7xx.c
@@ -209,4 +209,5 @@ static int __init npcm7xx_timer_init(struct device_node *np)
 }
 
 TIMER_OF_DECLARE(npcm7xx, "nuvoton,npcm750-timer", npcm7xx_timer_init);
+TIMER_OF_DECLARE(npcm8xx, "nuvoton,npcm845-timer", npcm7xx_timer_init);
 
